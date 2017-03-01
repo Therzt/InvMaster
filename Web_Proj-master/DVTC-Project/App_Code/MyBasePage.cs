d59a9459fc90cd29c3dc4b6cb9533a0f828fcc66
Eli <eziaiefar@gmail.com>
2017-03-01 16:27:03 -0600

    1st push

---
+++ Web_Proj-master/DVTC-Project/App_Code/MyBasePage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.UI;
+
+/// <summary>
+/// Summary description for MyBasePage
+/// </summary>
+public class MyBasePage : Page
+{
+    public MyBasePage()
+    {
+        PreRender += Page_PreRender;
+    }
+    private void Page_PreRender(object sender, EventArgs e)
+    {
+        if (string.IsNullOrEmpty(Title))
+        {
+            Title = "DVT&C Scanner App";
+        }
+    }
+}
